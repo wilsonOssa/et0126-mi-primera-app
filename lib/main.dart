be4b2c7c4237aadf242701d6fc3ecf745a73d1ce
wilson <wilsonossa15@gmail.com>
2026-08-24 01:44:19 -0500

    style: cambiar el color del tema principal a teal

--- lib/main.dart
+++ lib/main.dart
@@ -28,7 +28,7 @@ class MyApp extends StatelessWidget {
         //
         // This works for code too, not just values: Most code changes can be
         // tested with just a hot reload.
-        colorScheme: .fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: .fromSeed(seedColor: Colors.teal),
       ),
       home: const MyHomePage(title: 'App de Wilson - Mi Primera App'),
     );
